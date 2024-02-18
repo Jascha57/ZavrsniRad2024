704c91b6c8f4e421aa03da7eeb43da491df3ae2f
Jascha57 <jakov.petric.student@gmail.com>
2024-02-18 18:43:35 +0100

    Prva iteracija zavrsnog rada.

---
+++ project/website/urls.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from django.urls import path, re_path
+from . import views
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
