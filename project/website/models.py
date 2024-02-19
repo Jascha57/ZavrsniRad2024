d77aaa2a5f34d819a80d244a0f82997132335381
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 20:27:45 +0100

    Refactor models.py to add default values and auto_now_add fields

--- project/website/models.py
+++ project/website/models.py
@@ -3,9 +3,9 @@ from users.models import CustomUser
 
 class Event(models.Model):
     slug = models.SlugField(unique=True, null=False, blank=False, default='Event-Slug')
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    date = models.DateTimeField()
+    title = models.CharField(max_length=100, default='Title', blank=False, null=False)
+    description = models.TextField(default='Description', blank=False, null=False)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
@@ -13,9 +13,10 @@ class Event(models.Model):
 class News(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True, null=False, blank=False, default='News-Slug')
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    date = models.DateTimeField()
+    title = models.CharField(max_length=100, default='Title', blank=False, null=False)
+    description = models.TextField(default='Description', blank=False, null=False)
+    short_description = models.TextField(max_length=100, default='Short description', blank=False, null=False)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
