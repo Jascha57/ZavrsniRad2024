2889f43ee3401fcddb48c1906edf30d4431baa41
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 19:49:58 +0100

    Add Event and News models

--- project/website/models.py
+++ project/website/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+class News(models.Model):
+    author = models.ForeignKey(get_user_model, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
