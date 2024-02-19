d43afd74e006eda36c8ad2cf884b4188512757f5
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 22:45:08 +0100

    Add CustomUserAdmin class to admin.py, and setting up user registration part of the website.

--- project/users/admin.py
+++ project/users/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import CustomUser
 from django.contrib.auth.models import Group
 
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+
+    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active',)
+    search_fields = ('email', 'first_name', 'last_name',)
+    exclude = ('password',)
+
+    def has_add_permission(self, request):
+        return False
+
 class GroupAdmin(admin.ModelAdmin):
     pass
 
@@ -11,6 +21,6 @@ class GroupProxy(Group):
         verbose_name = 'Role'
         verbose_name_plural = 'Roles'
 
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.unregister(Group)
 admin.site.register(GroupProxy, GroupAdmin)
