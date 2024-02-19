9d8e1ad52ecf5164767f321244c92ca9d5d0de6d
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 20:43:43 +0100

    Add admin classes for Event and News models

--- project/website/admin.py
+++ project/website/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'slug', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'slug', 'description', 'short_description')
+    prepopulated_fields = {'slug': ('title',)}
+
 admin.site.register(Event)
 admin.site.register(News)
--- project/website/models.py
+++ project/website/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from users.models import CustomUser
 
 class Event(models.Model):
-    slug = models.SlugField(unique=True, null=False, blank=False, default='Event-Slug')
+    slug = models.SlugField(unique=True, null=False, blank=False, default='')
     title = models.CharField(max_length=100, default='Title', blank=False, null=False)
     description = models.TextField(default='Description', blank=False, null=False)
     date = models.DateTimeField(auto_now_add=True)
@@ -12,7 +12,7 @@ class Event(models.Model):
 
 class News(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    slug = models.SlugField(unique=True, null=False, blank=False, default='News-Slug')
+    slug = models.SlugField(unique=True, null=False, blank=False, default='')
     title = models.CharField(max_length=100, default='Title', blank=False, null=False)
     description = models.TextField(default='Description', blank=False, null=False)
     short_description = models.TextField(max_length=100, default='Short description', blank=False, null=False)
