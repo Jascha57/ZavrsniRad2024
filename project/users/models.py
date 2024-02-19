764c8d557404fb75309b20367de3c5cef0fda195
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 22:39:03 +0100

    Add gender field to CustomUser model

--- project/users/models.py
+++ project/users/models.py
@@ -3,8 +3,16 @@ from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('X', 'Other'),   
+    )
+
     username = None
     email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default='X')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
