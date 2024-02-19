1045cbbd60c7ebf6b4d24a48aa9d95ffb406ae1a
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 21:20:43 +0100

    Add GroupProxy and GroupAdmin classes to users/admin.py

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,4 +1,16 @@
 from django.contrib import admin
 from .models import CustomUser
+from django.contrib.auth.models import Group
+
+class GroupAdmin(admin.ModelAdmin):
+    pass
+
+class GroupProxy(Group):
+    class Meta:
+        proxy = True
+        verbose_name = 'Role'
+        verbose_name_plural = 'Roles'
 
 admin.site.register(CustomUser)
+admin.site.unregister(Group)
+admin.site.register(GroupProxy, GroupAdmin)
