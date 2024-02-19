5a3984521cff9103cd48f50343735da86766cf71
Jascha57 <jakov.petric.student@gmail.com>
2024-02-19 21:14:29 +0100

    Add Jazzmin, CKEditor, and customizations*** ***Update URLs to include CKEditor and static media files*** ***Replace TextField with CKEditor5Field in Event and News models*** ***Add CKEditor CSS to base.html

--- project/website/models.py
+++ project/website/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from users.models import CustomUser
+from django_ckeditor_5.fields import CKEditor5Field
 
 class Event(models.Model):
     slug = models.SlugField(unique=True, null=False, blank=False, default='')
     title = models.CharField(max_length=100, default='Title', blank=False, null=False)
-    description = models.TextField(default='Description', blank=False, null=False)
+    description = CKEditor5Field('Text', config_name='extends')
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -14,7 +15,7 @@ class News(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     slug = models.SlugField(unique=True, null=False, blank=False, default='')
     title = models.CharField(max_length=100, default='Title', blank=False, null=False)
-    description = models.TextField(default='Description', blank=False, null=False)
+    description = CKEditor5Field('Text', config_name='extends')
     short_description = models.TextField(max_length=100, default='Short description', blank=False, null=False)
     date = models.DateTimeField(auto_now_add=True)
 
