018296395b3a7f4a898a806848c83ed35c48d1c1
Jascha57 <jakov.petric.student@gmail.com>
2024-02-20 19:21:34 +0100

    Add news page and update navbar link

--- project/website/urls.py
+++ project/website/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('news/', views.news, name='news'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- project/website/views.py
+++ project/website/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+from .models import *
+
 def homepage(request):
     return render(request, 'homepage.html')
+
+def news(request):
+    news = News.objects.all()
+    return render(request, 'news.html', {'news': news})
