684274de82bd257d41f66ebbe90047878b832a1a
Jascha57 <jakov.petric.student@gmail.com>
2024-02-22 22:37:20 +0100

    Refactor image_upload_to method and add default profile picture handling

--- project/users/models.py
+++ project/users/models.py
@@ -6,10 +6,8 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
 
-    def image_upload_to(self, instance=None):
-        if instance:
-            return os.path.join("Users", self.username, instance)
-        return None
+    def image_upload_to(self, filename):
+        return 'users/user_{0}/{1}'.format(self.id, filename)
 
     GENDER_CHOICES = (
         ('M', 'Male'),
@@ -20,12 +18,17 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default='X')
-    profile_picture = models.ImageField(default='default/user.jpg', upload_to=image_upload_to)
+    profile_picture = models.ImageField(default='default/user.jpg', upload_to=image_upload_to, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+    def save(self, *args, **kwargs):
+        if not self.profile_picture:
+            self.profile_picture = 'default/user.jpg'
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.email
