d7e3df71273b8ea5941d57e68f00a213aab8dbae
Jascha57 <jakov.petric.student@gmail.com>
2024-02-22 22:09:41 +0100

    Add profile picture styling and display in navbar

--- project/users/admin.py
+++ project/users/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin.widgets import AdminFileWidget
+from django.utils.safestring import mark_safe
+
 from .models import CustomUser
 from django.contrib.auth.models import Group
+
+class AdminImageWidget(AdminFileWidget):
+    def render(self, name, value, attrs=None, renderer=None):
+        output = []
+        if value and getattr(value, "url", None):
+            image_url = value.url
+            output.append(f'<a href="{image_url}" target="_blank"><img src="{image_url}" alt="profile picture" width="50" height="50" style="object-fit: cover;"/></a>')
+        output.append(super().render(name, value, attrs))
+        return mark_safe(''.join(output))
 
 class CustomUserAdmin(admin.ModelAdmin):
     model = CustomUser
@@ -11,6 +23,13 @@ class CustomUserAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, request):
         return False
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'profile_picture':
+            request = kwargs.pop("request", None)
+            kwargs['widget'] = AdminImageWidget
+            return db_field.formfield(**kwargs)
+        return super().formfield_for_dbfield(db_field, **kwargs)
 
 class GroupAdmin(admin.ModelAdmin):
     pass
--- project/users/models.py
+++ project/users/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import os
+
 from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
+
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join("Users", self.username, instance)
+        return None
 
     GENDER_CHOICES = (
         ('M', 'Male'),
@@ -13,6 +20,7 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=2, choices=GENDER_CHOICES, default='X')
+    profile_picture = models.ImageField(default='default/user.jpg', upload_to=image_upload_to)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
