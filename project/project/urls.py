0c46e82f93c031e647b5a271a34fca34b653bcae
Jascha57 <jakov.petric.student@gmail.com>
2024-02-24 12:47:45 +0100

    Update urls.py to serve media files using re_path

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
@@ -8,4 +8,4 @@ urlpatterns = [
     path('', include('users.urls')),
     path('', include('website.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls'), name="ck_editor_5_upload_file"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
