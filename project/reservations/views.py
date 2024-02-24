02973444c4978eb20d269b5872ee2a6aa46b33c6
Jascha57 <jakov.petric.student@gmail.com>
2024-02-24 15:17:28 +0100

    Add service details and doctors to services.html

--- project/reservations/views.py
+++ project/reservations/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Group
 
 from website.models import Services
+from users.models import CustomUser
 
 def services(request):
     services = Services.objects.all()
+    for service in services:
+        group_name = service.title + ' - Service'
+        group = Group.objects.get(name=group_name)
+        service.doctors = CustomUser.objects.filter(groups__name=group_name)
     return render(request, 'services.html', {'services': services})
 
 def reservations(request):
